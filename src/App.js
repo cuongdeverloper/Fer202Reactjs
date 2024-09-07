357538af917ec69ee5fb7f80089b8768a6470ca8
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2024-09-07 10:34:52 +0700

    commit1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <span>CUONG NGU</span>
         </p>
         <a
           className="App-link"
