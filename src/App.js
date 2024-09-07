7e3019d6781209f2b1599cc4d59116aae90e2918
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2024-09-07 11:23:53 +0700

    deptrai

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <span>CUONG NGUs</span>
+          <span>Khoa dep trai</span>
         </p>
         <a
           className="App-link"
